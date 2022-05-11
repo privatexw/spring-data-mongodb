c503f6f28eedaabbbd70375f11fdad8688289a18
xw <private_xw@163.com>
2022-05-11 23:09:29 +0800

    master commit2!

--- src/main/java/com/xw/GitTest.java
+++ src/main/java/com/xw/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hot-fix commit1");
+        System.out.println("master commit2");
     }
 }
